f6acff806e9f29efc14641d2d89af34a6bde9314
Lektro <kevin.vanderkruk@gmail.com>
2023-07-21 01:36:00 +0200

    batch processing working

--- src/main/java/org/example/AnomalyDetectionHotPixels.java
+++ src/main/java/org/example/AnomalyDetectionHotPixels.java
@@ -6,6 +6,7 @@ import nom.tam.fits.FitsException;
 import nom.tam.fits.ImageHDU;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,18 @@ public class AnomalyDetectionHotPixels {
 
     public static void main(String[] args) {
         // Replace "path/to/your/image.fits" with the actual file path of the FITS image
-        String fitsFilePath = "C:/Temp/jw04499-o080_t002_miri_f770w_i2d.fits";
+        //String fitsFilePath = "C:/Temp/jw02733-o001_t001_nircam_clear-f090w_i2d.fits";
+
+
+        // Replace "path/to/your/folder" with the folder containing the FITS files
+        String folderPath = "C:/Temp/DataInput";
+
+        // Create a folder to store the output images if it doesn't exist
+        String outputFolder = "C:/Temp/DataOutput";
+        File outputDir = new File(outputFolder);
+        if (!outputDir.exists()) {
+            outputDir.mkdir();
+        }
 
         // Replace this value with an appropriate threshold
         double hotPixelThreshold = 550.0;
@@ -26,113 +38,135 @@ public class AnomalyDetectionHotPixels {
         // Adjust the marker size as needed
         int hotPixelMarkerSize = 15;
 
-        try {
-            // Step 1: Read FITS image data
-            Fits fits = new Fits(fitsFilePath);
-            //FitsDataReader fitsDataReader = new FitsDataReader(fitsFilePath);
+
+        // Get a list of all FITS files in the specified folder
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".fits"));
+
+        if (files == null || files.length == 0) {
+            System.out.println("No FITS files found in the folder.");
+            return;
+        }
+
+        for (File file : files) {
+            String fitsFilePath = file.getAbsolutePath();
+
+            try {
+                // Step 1: Read FITS image data
+                Fits fits = new Fits(fitsFilePath);
 
 
+                // Debug information: Print the number of HDUs in the FITS file
+                int numHDUs = fits.getNumberOfHDUs();
+                System.out.println("Number of HDUs in the FITS file: " + numHDUs);
 
-
-            // Debug information: Print the number of HDUs in the FITS file
-            int numHDUs = fits.getNumberOfHDUs();
-            System.out.println("Number of HDUs in the FITS file: " + numHDUs);
-
-            // Print information about each HDU in the FITS file
-            for (int i = 0; i < numHDUs; i++) {
-                BasicHDU<?> hdu = fits.getHDU(i);
-                System.out.println("HDU " + i + " type: " + hdu.getClass().getSimpleName());
-                if (hdu instanceof ImageHDU) {
-                    ImageHDU imageHDU = (ImageHDU) hdu;
-                    System.out.println("Image axes: " + imageHDU.getAxes()[0] + " x " + imageHDU.getAxes()[1]);
-                }
-            }
-
-            // Get the primary HDU index starts at 1 , not 0
-            BasicHDU<?> hdu = fits.getHDU(1);
-            if (hdu == null) {
-                System.err.println("Error: FITS imageHDU is null or empty.");
-                return;
-            }
-
-            if (!(hdu instanceof ImageHDU)) {
-                System.err.println("Error: The primary HDU is not an ImageHDU.");
-                return;
-            }
-
-            ImageHDU imageHDU = (ImageHDU) hdu;
-            Object dataObject = imageHDU.getData().getData();
-
-            double[][] data = convertToDoubleArray(dataObject);
-
-            // Get the image data as a 2D array
-            //double[][] data = (double[][]) imageHDU.getData().getData();
-
-            // Verify the dimensions of the image data
-            //int imageWidth = data[0].length;
-            //int imageHeight = data[0].length;
-
-            // Ensure data is not null before proceeding
-            if (data == null) {
-                System.err.println("Error: FITS data is null or empty.");
-                return;
-            }
-
-            // Step 2: Identify hot pixels (simple anomaly detection example)
-            List<HotPixel> hotPixels = new ArrayList<>();
-
-            int imageWidth = imageHDU.getAxes()[1];
-            int imageHeight = imageHDU.getAxes()[1];
-
-            for (int y = 0; y < imageHeight; y++) {
-                for (int x = 0; x < imageWidth; x++) {
-                    double pixelValue = getPixelValue(data, x, y);
-                    if (pixelValue >= hotPixelThreshold) {
-                        hotPixels.add(new HotPixel(x, y, pixelValue));
+                // Print information about each HDU in the FITS file
+                for (int i = 0; i < numHDUs; i++) {
+                    BasicHDU<?> hdu = fits.getHDU(i);
+                    System.out.println("HDU " + i + " type: " + hdu.getClass().getSimpleName());
+                    if (hdu instanceof ImageHDU) {
+                        ImageHDU imageHDU = (ImageHDU) hdu;
+                        System.out.println("Image axes: " + imageHDU.getAxes()[0] + " x " + imageHDU.getAxes()[1]);
                     }
                 }
+
+                // Get the primary HDU index starts at 1 , not 0
+                BasicHDU<?> hdu = fits.getHDU(1);
+                if (hdu == null) {
+                    System.err.println("Error: FITS imageHDU is null or empty.");
+                    return;
+                }
+
+                if (!(hdu instanceof ImageHDU)) {
+                    System.err.println("Error: The primary HDU is not an ImageHDU.");
+                    return;
+                }
+
+                ImageHDU imageHDU = (ImageHDU) hdu;
+                Object dataObject = imageHDU.getData().getData();
+
+                double[][] data = convertToDoubleArray(dataObject);
+
+                // Get the image data as a 2D array
+                //double[][] data = (double[][]) imageHDU.getData().getData();
+
+                // Verify the dimensions of the image data
+                //int imageWidth = data[0].length;
+                //int imageHeight = data[0].length;
+
+                // Ensure data is not null before proceeding
+                if (data == null) {
+                    System.err.println("Error: FITS data is null or empty.");
+                    return;
+                }
+
+                // Step 2: Identify hot pixels (simple anomaly detection example)
+                List<HotPixel> hotPixels = new ArrayList<>();
+
+                int imageWidth = imageHDU.getAxes()[1];
+                int imageHeight = imageHDU.getAxes()[1];
+
+                for (int y = 0; y < imageHeight; y++) {
+                    for (int x = 0; x < imageWidth; x++) {
+                        double pixelValue = getPixelValue(data, x, y);
+                        if (pixelValue >= hotPixelThreshold) {
+                            hotPixels.add(new HotPixel(x, y, pixelValue));
+                        }
+                    }
+                }
+
+                // Enhance the image (choose the desired enhancement method)
+                double[][] enhancedImage = ImageEnhancer.contrastStretch(data); // or ImageEnhancer.histogramEqualization(data);
+
+                // Subtract background from the enhanced image
+                double[][] backgroundSubtractedImage = BackgroundSubtractor.subtractBackground(enhancedImage);
+
+                // Calibrate the background-subtracted image
+                double[][] calibratedImage = ImageCalibrator.calibrateImage(backgroundSubtractedImage);
+
+
+                // Output detected hot pixels to console
+                System.out.println("Number of hot pixels detected: " + hotPixels.size());
+                for (HotPixel hotPixel : hotPixels) {
+                    System.out.println("Hot pixel at (x=" + hotPixel.getX() + ", y=" + hotPixel.getY() +
+                            ") with value: " + hotPixel.getValue());
+                }
+
+                // Step 3: Process hot pixels and save the marked image
+                if (!hotPixels.isEmpty()) {
+                    // Create a folder for each file in the output directory
+                    String fileName = file.getName();
+                    String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
+                    String fileOutputFolder = outputFolder + "/" + fileNameWithoutExtension;
+                    File fileOutputDir = new File(fileOutputFolder);
+                    if (!fileOutputDir.exists()) {
+                        fileOutputDir.mkdir();
+                    }
+
+                    // Save the marked image as JPEG with the original name
+                    String markedImageFilePath = fileOutputFolder + "/marked_" + fileNameWithoutExtension + ".jpg";
+                    FitsImage fitsImage = new FitsImage(data, imageWidth, imageHeight, hotPixels);
+                    HotPixelMarker.markHotPixelsAndSave(fitsImage, hotPixelMarkerSize, markedImageFilePath);
+
+                    // Save the original image as JPEG with the original name
+                    String originalImageFilePath = fileOutputFolder + "/original_" + fileNameWithoutExtension + ".jpg";
+                    saveFitsImageAsJpeg(data, imageWidth, imageHeight, originalImageFilePath);
+
+                    // Save the enhanced image as JPEG with the original name
+                    String enhancedImageFilePath = fileOutputFolder + "/enhanced_" + fileNameWithoutExtension + ".jpg";
+                    saveFitsImageAsJpeg(enhancedImage, imageWidth, imageHeight, enhancedImageFilePath);
+
+                    System.out.println("Images for file '" + fileName + "' processed and saved.");
+                } else {
+                    System.out.println("No hot pixels detected in the image.");
+                }
+
+
+            } catch (FitsException | IOException e) {
+                e.printStackTrace();
             }
-
-            // Enhance the image (choose the desired enhancement method)
-            double[][] enhancedImage = ImageEnhancer.contrastStretch(data); // or ImageEnhancer.histogramEqualization(data);
-
-            // Subtract background from the enhanced image
-            double[][] backgroundSubtractedImage = BackgroundSubtractor.subtractBackground(enhancedImage);
-
-            // Calibrate the background-subtracted image
-            double[][] calibratedImage = ImageCalibrator.calibrateImage(backgroundSubtractedImage);
-
-
-            // Output detected hot pixels to console
-            System.out.println("Number of hot pixels detected: " + hotPixels.size());
-            for (HotPixel hotPixel : hotPixels) {
-                System.out.println("Hot pixel at (x=" + hotPixel.getX() + ", y=" + hotPixel.getY() +
-                        ") with value: " + hotPixel.getValue());
-            }
-
-            // Step 3: Process hot pixels and save the marked image
-            if (!hotPixels.isEmpty()) {
-                FitsImage fitsImage = new FitsImage(data, imageWidth, imageHeight, hotPixels);
-
-
-                HotPixelMarker.markHotPixelsAndSave(fitsImage, 15, "C:/Temp/marked_image.jpg");
-
-                // Save the original image as JPEG
-                saveFitsImageAsJpeg(data, imageWidth, imageHeight, "C:/Temp/original_image.jpg");
-
-                // Save the enhanced image as JPEG
-                saveFitsImageAsJpeg(data, imageWidth, imageHeight, "C:/Temp/enhanced_image.jpg");
-
-            } else {
-                System.out.println("No hot pixels detected in the image.");
-            }
-
-
-        } catch (FitsException | IOException e) {
-            e.printStackTrace();
         }
     }
-
 
     // Utility method to get the pixel value from the image data
     private static double getPixelValue(Object data, int x, int y) {
--- src/main/java/org/example/FitsImageProcessor.java
+++ src/main/java/org/example/FitsImageProcessor.java
@@ -1,29 +1,3 @@
-/*
 package org.example;
 
-import nom.tam.fits.FitsException;
 
-import java.io.IOException;
-import java.util.List;
-
-public class FitsImageProcessor {
-    private double[][] enhancedData;
-    private List<HotPixel> hotPixels;
-
-    public FitsImageProcessor(String fitsFilePath, double hotPixelThreshold) throws FitsException, IOException {
-        // Step 1: Read FITS image data using the FitsDataReader
-        FitsDataReader fitsDataReader = new FitsDataReader(fitsFilePath);
-
-        // Step 2: Enhance the original image in chunks
-        // ... Other code ...
-
-        // Step 3: Detect hot pixels in the enhanced image
-        // ... Other code ...
-
-        this.enhancedData = enhancedData;
-        this.hotPixels = hotPixels;
-    }
-
-    // Getters for enhancedData and hotPixels
-}
-*/
