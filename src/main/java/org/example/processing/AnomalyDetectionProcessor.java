65e659f9bbd98481e85779e582f3784a7d202d4f
Lektro <kevin.vanderkruk@gmail.com>
2023-07-21 22:01:39 +0200

    refactoring in main and processor

--- src/main/java/org/example/processing/AnomalyDetectionProcessor.java
+++ src/main/java/org/example/processing/AnomalyDetectionProcessor.java
@@ -4,7 +4,6 @@ import nom.tam.fits.BasicHDU;
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
 import nom.tam.fits.ImageHDU;
-import org.example.models.FitsImage;
 import org.example.models.HotPixel;
 
 import javax.imageio.ImageIO;
@@ -35,57 +34,73 @@ public class AnomalyDetectionProcessor {
         this.hotPixelMarkerSize = hotPixelMarkerSize;
     }
 
-    // Method to process and save the images
+    // Public method to initiate the processing and saving of images
     public void processAndSaveImages() throws FitsException, IOException {
         try {
             // Create a Fits object to read the FITS file
             Fits fits = new Fits(fitsFilePath);
 
+            // Get the number of Header Data Units (HDUs) in the FITS file
+            int numHDUs = fits.getNumberOfHDUs();
 
-        // Get the number of Header Data Units (HDUs) in the FITS file
-        int numHDUs = fits.getNumberOfHDUs();
+            // Loop through all HDUs in the FITS file
+            for (int i = 0; i < numHDUs; i++) {
+                BasicHDU<?> hdu = fits.getHDU(i);
+                System.out.println("HDU " + i + " type: " + hdu.getClass().getSimpleName());
 
-        // Loop through all HDUs in the FITS file
-        for (int i = 0; i < numHDUs; i++) {
-            BasicHDU<?> hdu = fits.getHDU(i);
-            System.out.println("HDU " + i + " type: " + hdu.getClass().getSimpleName());
-
-            // If the HDU is an ImageHDU, print its image axes
-            if (hdu instanceof ImageHDU) {
-                ImageHDU imageHDU = (ImageHDU) hdu;
-                System.out.println("Image axes: " + imageHDU.getAxes()[0] + " x " + imageHDU.getAxes()[1]);
+                // If the HDU is an ImageHDU, print its image axes
+                if (hdu instanceof ImageHDU) {
+                    ImageHDU imageHDU = (ImageHDU) hdu;
+                    System.out.println("Image axes: " + imageHDU.getAxes()[0] + " x " + imageHDU.getAxes()[1]);
+                }
             }
-        }
 
+            // Get the primary HDU, which is typically the second HDU in the FITS file (index 1)
+            BasicHDU<?> hdu = fits.getHDU(1);
 
-        // Get the primary HDU, which is typically the second HDU in the FITS file (index 1)
-        BasicHDU<?> hdu = fits.getHDU(1);
-
-        // Check if the primary HDU is null or not an ImageHDU
-        if (hdu == null) {
-            System.err.println("Error: FITS imageHDU is null or empty.");
-            return;
-        }
+            // Check if the primary HDU is null or not an ImageHDU
+            if (hdu == null) {
+                System.err.println("Error: FITS imageHDU is null or empty.");
+                return;
+            }
 
             // Check if the primary HDU is not an ImageHDU, and handle the error
             if (!(hdu instanceof ImageHDU)) {
-            System.err.println("Error: The primary HDU is not an ImageHDU.");
-            return;
+                System.err.println("Error: The primary HDU is not an ImageHDU.");
+                return;
+            }
+
+            // Cast the primary HDU to an ImageHDU
+            ImageHDU imageHDU = (ImageHDU) hdu;
+
+            // Get the data from the ImageHDU as a 2D array
+            Object dataObject = imageHDU.getData().getData();
+            double[][] data = convertToDoubleArray(dataObject);
+
+            // Detect hot pixels and store them in a list
+            List<HotPixel> hotPixels = detectHotPixels(data);
+
+            // Enhance the image and save processed images
+            String outputFilePath = "/path/to/output/image.jpg"; // Set the output file path here
+            saveFitsImageAsJpeg(data, imageHDU.getAxes()[1], imageHDU.getAxes()[0], hotPixels, outputFilePath);
+
+
+        } catch (FitsException e) {
+            // Handle the FitsException here, for example, log the error or show an error message
+            e.printStackTrace();
+        } catch (IOException e) {
+            // Handle any other IOException that might occur during file handling
+            e.printStackTrace();
         }
+    }
 
-        // Cast the primary HDU to an ImageHDU
-        ImageHDU imageHDU = (ImageHDU) hdu;
-
-        // Get the data from the ImageHDU as a 2D array
-        Object dataObject = imageHDU.getData().getData();
-        double[][] data = convertToDoubleArray(dataObject);
-
-        // Create a list to store the detected hot pixels
+    // Utility method to detect hot pixels in the image
+    private List<HotPixel> detectHotPixels(double[][] data) {
         List<HotPixel> hotPixels = new ArrayList<>();
 
         // Get the dimensions of the image
-        int imageWidth = imageHDU.getAxes()[1];
-        int imageHeight = imageHDU.getAxes()[0];
+        int imageWidth = data[0].length;
+        int imageHeight = data.length;
 
         // Loop through all pixels in the image and detect hot pixels
         for (int y = 0; y < imageHeight; y++) {
@@ -97,19 +112,6 @@ public class AnomalyDetectionProcessor {
             }
         }
 
-        // Initialize enhancedData with the original data
-        double[][] enhancedData = data;
-
-        // Apply contrast stretching to enhance the image
-        enhancedData = ImageEnhancer.contrastStretch(enhancedData);
-
-        // Apply sharpening to enhance the image
-        enhancedData = ImageEnhancer.sharpen(enhancedData);
-
-        // Apply de-noising to enhance the image
-        enhancedData = ImageEnhancer.denoise(enhancedData);
-
-
         // Print the number of hot pixels detected and their coordinates and values in the console
         System.out.println("Number of hot pixels detected: " + hotPixels.size());
         for (HotPixel hotPixel : hotPixels) {
@@ -117,77 +119,7 @@ public class AnomalyDetectionProcessor {
                     ") with value: " + hotPixel.getValue());
         }
 
-        // If hot pixels are detected, save the processed images
-        if (!hotPixels.isEmpty()) {
-
-            String fileName = new File(fitsFilePath).getName();
-
-            String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-            String fileOutputFolder = outputFolder + "/" + fileNameWithoutExtension;
-
-            File fileOutputDir = new File(fileOutputFolder);
-
-            // Create a folder for each file in the output directory if it doesn't exist
-            if (!fileOutputDir.exists()) {
-                fileOutputDir.mkdir();
-            }
-
-            // File paths for the marked, original, and enhanced images
-            String markedImageFilePath = fileOutputFolder + "/marked_" + fileNameWithoutExtension + ".jpg";
-            String originalImageFilePath = fileOutputFolder + "/original_" + fileNameWithoutExtension + ".jpg";
-            String enhancedImageFilePath = fileOutputFolder + "/enhanced_" + fileNameWithoutExtension + ".jpg";
-
-            // Create a FitsImage object to hold the data and hot pixels for the marked image
-            FitsImage fitsImage = new FitsImage(data, imageWidth, imageHeight, hotPixels);
-
-            // Mark hot pixels in the image and save the marked image
-            HotPixelMarker.markHotPixelsAndSave(fitsImage, hotPixelMarkerSize, markedImageFilePath);
-
-            // Save the original image as JPEG
-            saveFitsImageAsJpeg(data, imageWidth, imageHeight, originalImageFilePath);
-
-            // Save the enhanced image as JPEG
-            saveFitsImageAsJpeg(enhancedData, imageWidth, imageHeight, enhancedImageFilePath);
-
-            // debug: see if the image was processed and saved in the console
-            System.out.println("Images for file '" + fileName + "' processed and saved.");
-
-        } else {
-
-            // If no hot pixels are detected, save the original image as JPEG only
-            System.out.println("No hot pixels detected in the image.");
-
-            // Extract the file name and remove the file extension to create a new output folder
-            String fileName = new File(fitsFilePath).getName();
-
-            String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-            String fileOutputFolder = outputFolder + "/" + fileNameWithoutExtension;
-
-            // Create a file object for the output folder
-            File fileOutputDir = new File(fileOutputFolder);
-
-            // Create a folder for each file in the output directory if it doesn't exist
-            if (!fileOutputDir.exists()) {
-                fileOutputDir.mkdir();
-            }
-
-            // Save the original image as JPEG in the output folder
-            String originalImageFilePath = fileOutputFolder + "/original_" + fileNameWithoutExtension + ".jpg";
-            System.out.println("Data array length: " + data.length); // Check the length of the data array
-            saveFitsImageAsJpeg(data, imageWidth, imageHeight, originalImageFilePath);
-
-            // Print save status message in terminal
-            System.out.println("Images for file '" + fileName + "' processed and saved.");}
-
-        } catch (FitsException e) {
-
-            // Handle the FitsException here, for example, log the error or show an error message
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            // Handle any other IOException that might occur during file handling
-            e.printStackTrace();
-        }
+        return hotPixels;
     }
 
     /**
@@ -235,12 +167,18 @@ public class AnomalyDetectionProcessor {
      * Utility method to save the FITS image as JPEG
      * Saves the FITS image as a JPEG file with the specified pixel data and dimensions.
      *
-     * @param data          The 2D array containing the pixel values of the FITS image.
-     * @param width         The width of the image in pixels.
-     * @param height        The height of the image in pixels.
+     * @param data           The 2D array containing the pixel values of the FITS image.
+     * @param width          The width of the image in pixels.
+     * @param height         The height of the image in pixels.
+     * @param hotPixels
      * @param outputFilePath The file path where the JPEG image will be saved.
      */
-    private static void saveFitsImageAsJpeg(double[][] data, int width, int height, String outputFilePath) {
+    private static void saveFitsImageAsJpeg(double[][] data, int width, int height, List<HotPixel> hotPixels, String outputFilePath) {
+        // Check if the data array is null or has invalid dimensions
+        if (data == null || data.length != height || data[0].length != width) {
+            System.err.println("Error: Invalid data array or dimensions.");
+            return;
+        }
 
         // Create a new BufferedImage with the specified width and height
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -272,7 +210,12 @@ public class AnomalyDetectionProcessor {
             // Create a new File object representing the output file path
             File outputFile = new File(outputFilePath);
 
-            // Write the BufferedImage as a your chosen image format to the output file
+            // Check if the parent directories exist, if not, create them
+            if (!outputFile.getParentFile().exists()) {
+                outputFile.getParentFile().mkdirs();
+            }
+
+            // Write the BufferedImage as your chosen image format to the output file
             ImageIO.write(image, imageFormat, outputFile);
 
             // Print a message to indicate that the image has been saved successfully
